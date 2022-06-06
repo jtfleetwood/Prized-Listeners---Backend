e2c3b5b9dfd401a705a90e91aaeee548a924c00c
JT <jfleetw@clemson.edu>
2022-06-06 14:57:57 -0400

    Fixed misc. error smh...

--- routers/users_router.js
+++ routers/users_router.js
@@ -8,6 +8,6 @@ users_router.get('/', async (req, res) => {
     res.send(await auth0_services.get_users())
 });
 
-users_router.get('/:id/check_vote', (req, res) => {
+users_router.get('/:id/check_vote', async (req, res) => {
     res.send(await auth0_services.get_user_vote(req.params.id));
 })
